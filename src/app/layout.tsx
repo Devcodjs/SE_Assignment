9411aa82f0c1a189e51f8fb9764b7059dbf26aea
hirakRajbonshi <hrkraj0@gmail.com>
2025-11-11 19:47:23 +0530

    components move to src from app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import About from "./components/About";
-import FAQ from "./components/FAQ";
-import Prizes from "./components/Prizes";
-import Timeline from "./components/Timeline";
+import About from "~/components/About";
+import FAQ from "~/components/FAQ";
+import Prizes from "~/components/Prizes";
+import Timeline from "~/components/Timeline";
 
 export default function HomePage() {
   return (
